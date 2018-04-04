4b7b65ac3f612b437b757dcc54be3ad5198a7c81
Williams <williamsalcantara@gmail.com>
2018-04-03 23:47:14 -0300

    Interface provida

--- ReservaMgt/src/br/ufal/ic/ppgi/smartagenda/reservamgt/spec/prov/IReservaMgt.java
+++ ReservaMgt/src/br/ufal/ic/ppgi/smartagenda/reservamgt/spec/prov/IReservaMgt.java
@@ -6,9 +6,12 @@ import br.ufal.ic.ppgi.smartagenda.reservamgt.spec.dt.Reserva;
 
 public interface IReservaMgt {
 
-	public List<Reserva> consultarList(int codItem);
+	public List<Reserva> consultarLista(int codItem);
 	public void atualizarLista(List<Reserva> reservas);
-	public Reserva consultarReserva(int codReserva);
+	
+	public Reserva criarReserva(int codItem, int codUsuario);
+	public Reserva consultarReserva(int codReserva, int codItem, int codUsuario);
 	public Reserva cancelarReserva(int codReserva);
+	public Reserva finalizarReserva(int codReserva);
 
 }
