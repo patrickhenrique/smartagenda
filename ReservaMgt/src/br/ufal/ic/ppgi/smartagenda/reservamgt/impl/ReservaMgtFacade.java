3faca89a989fd2751b28116b9c7c6ce44912c8df
Williams <williamsalcantara@gmail.com>
2018-04-03 23:46:47 -0300

    fachada

--- ReservaMgt/src/br/ufal/ic/ppgi/smartagenda/reservamgt/impl/ReservaMgtFacade.java
+++ ReservaMgt/src/br/ufal/ic/ppgi/smartagenda/reservamgt/impl/ReservaMgtFacade.java
@@ -2,27 +2,46 @@ package br.ufal.ic.ppgi.smartagenda.reservamgt.impl;
 
 import java.util.List;
 
+import br.ufal.ic.ppgi.smartagenda.reservamgt.spec.dt.Item;
 import br.ufal.ic.ppgi.smartagenda.reservamgt.spec.dt.Reserva;
+import br.ufal.ic.ppgi.smartagenda.reservamgt.spec.dt.Usuario;
 import br.ufal.ic.ppgi.smartagenda.reservamgt.spec.prov.IReservaMgt;
 
 class ReservaMgtFacade implements IReservaMgt {
 
-	public List<Reserva> consultarList(int codItem) {
+	@Override
+	public List<Reserva> consultarLista(int codItem) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
+	@Override
 	public void atualizarLista(List<Reserva> reservas) {
 		// TODO Auto-generated method stub
 
 	}
-
-	public Reserva consultarReserva(int codReserva) {
+	
+	@Override
+	public Reserva criarReserva(int codItem, int codUsuario) {
 		// TODO Auto-generated method stub
 		return null;
 	}
+
+	@Override
+	public Reserva consultarReserva(int codReserva, int codUsuario, int codItem) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	@Override
 	public Reserva cancelarReserva(int codReserva) {
-		
+		// TODO Auto-generated method stub		
+		return null;
+	}
+
+	@Override
+	public Reserva finalizarReserva(int codReserva) {
+		// TODO Auto-generated method stub
 		return null;
 	}
 }
