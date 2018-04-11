365e1b794085a967653968a6797ac3567ae71571
Williams <williamsalcantara@gmail.com>
2018-04-10 23:43:55 -0300

    controle

---
+++ ReservaMgt/src/br/ufal/ic/ppgi/smartagenda/reservamgt/impl/ReservaControle.java
@@ -0,0 +1,42 @@
+package br.ufal.ic.ppgi.smartagenda.reservamgt.impl;
+
+import java.util.List;
+
+import br.ufal.ic.ppgi.smartagenda.reservamgt.spec.dt.Reserva;
+
+public class ReservaControle {
+
+	public List<Reserva> consultarLista(int codItem) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void atualizarLista(List<Reserva> reservas) {
+		// TODO Auto-generated method stub
+	}
+	
+	public Reserva criarReserva(int codItem, int codUsuario) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Reserva consultarReserva(int codReserva) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	public Reserva verificarDisponibilidade(int codItem, int codUsuario) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	public Reserva cancelarReserva(int codReserva) {
+		// TODO Auto-generated method stub		
+		return null;
+	}
+
+	public Reserva finalizarReserva(int codReserva) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
