2185dedce16b895650abdf1ba0cba73cd737b167
Christiano Rossini <christianorossinimc@gmail.com>
2018-06-10 05:00:26 -0300

    implementações

--- src/main/java/br/ufal/notificacoesops/impl/FacadeNotificacao.java
+++ src/main/java/br/ufal/notificacoesops/impl/FacadeNotificacao.java
@@ -5,12 +5,21 @@ import br.ufal.notificacoesops.spec.prov.INotificacaoOps;
 
 class FacadeNotificacao implements INotificacaoOps{
 
+	private Notificacao notificacao;
 	private IManager manager;
 	//private ILimiteOps intReq;
 	
+	
+	public FacadeNotificacao(IManager manager) {
+		this.manager = manager;
+		
+		this.notificacao = new Notificacao();
+	}
+
 
 	public void notificar() {
-		// TODO Auto-generated method stub
+		
+		this.notificacao.enviarNotificacao();
 		
 	}
 
